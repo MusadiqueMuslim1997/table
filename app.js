e48da493052dee97d457a3138e393564023001ff
Musadique Muslim <musadique1997@gmail.com>
2023-10-23 22:35:51 +0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,102 @@
+a = +prompt("Enter table number",5)
+document.write(a)
+var b = "x"
+document.write(b)
+var c = 1
+document.write(c)
+var d = "="
+document.write(d)
+var result = a*c
+document.write(result + "<br>")
+
+
+document.write(a)
+var b = "x"
+document.write(b)
+var c = 2
+document.write(c)
+var d = "="
+document.write(d)
+var result = a*c
+document.write(result + "<br>")
+
+document.write(a)
+var b = "x"
+document.write(b)
+var c = 3
+document.write(c)
+var d = "="
+document.write(d)
+var result = a*c
+document.write(result + "<br>")
+
+document.write(a)
+var b = "x"
+document.write(b)
+var c = 4
+document.write(c)
+var d = "="
+document.write(d)
+var result = a*c
+document.write(result + "<br>")
+
+document.write(a)
+var b = "x"
+document.write(b)
+var c = 5
+document.write(c)
+var d = "="
+document.write(d)
+var result = a*c
+document.write(result + "<br>")
+
+document.write(a)
+var b = "x"
+document.write(b)
+var c = 6
+document.write(c)
+var d = "="
+document.write(d)
+var result = a*c
+document.write(result + "<br>")
+
+document.write(a)
+var b = "x"
+document.write(b)
+var c = 7
+document.write(c)
+var d = "="
+document.write(d)
+var result = a*c
+document.write(result + "<br>")
+
+document.write(a)
+var b = "x"
+document.write(b)
+var c = 8
+document.write(c)
+var d = "="
+document.write(d)
+var result = a*c
+document.write(result + "<br>")
+
+document.write(a)
+var b = "x"
+document.write(b)
+var c = 9
+document.write(c)
+var d = "="
+document.write(d)
+var result = a*c
+document.write(result + "<br>")
+
+document.write(a)
+var b = "x"
+document.write(b)
+var c = 10
+document.write(c)
+var d = "="
+document.write(d)
+var result = a*c
+document.write(result + "<br>")
+
